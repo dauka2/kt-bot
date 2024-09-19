d4570a2fe4794b02adcbeb4962c254aa89f1bd9a
daulet <daulet.marat.2018@mail.ru>
2024-09-19 16:57:07 +0500

    Изменил админов кт-бота

--- db_connect.py
+++ db_connect.py
@@ -394,9 +394,9 @@ def insert_into_performers_right():
         'parent_category varchar(50))')
 
     cur.execute('insert into performers (performer_id, category, firstname, lastname, phone_num, email, telegram) '
-                'values (%s, %s, %s, %s, %s, %s, %s)', ("760906879", "Learning.telecom.kz | Техническая поддержка",
-                                                        "Дильназ", "Мустафина", "+77089081808",
-                                                          "info.ktcu@telecom.kz", "@dilnaz_mustafina"))
+                'values (%s, %s, %s, %s, %s, %s, %s)', ("6682886650", "Learning.telecom.kz | Техническая поддержка",
+                                                        "Тамирлан", "Оспанов", "87081930374", "info.ktcu@telecom.kz",
+                                                        "@ktbot_kazakhtelecom"))
     # cur.execute('insert into performers (performer_id, category, firstname, lastname, phone_num, email, telegram) '
     #             'values (%s, %s, %s, %s, %s, %s, %s)', ("1483219013", "Learning.telecom.kz | Техническая поддержка",
     #                                                     "Людмила", "Нам", "+77009145025", "info.ktcu@telecom.kz", "@"))
